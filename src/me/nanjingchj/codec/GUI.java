29e4a000147969d8ecaccfd8f5a560e83ea84016
nanjingchj <nanjingchj@gmail.com>
2020-01-20 17:13:10 -0500

    finished

--- src/me/nanjingchj/codec/GUI.java
+++ src/me/nanjingchj/codec/GUI.java
@@ -1,6 +1,13 @@
 package me.nanjingchj.codec;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Base64;
 
 public class GUI {
     private JPanel pnl;
@@ -14,13 +21,66 @@ public class GUI {
     public GUI() {
         btnEncode.addActionListener(e -> {
             String txtToEncode = this.txtToEncode.getText();
+            byte[] encodedBytes = txtToEncode.getBytes();
+            String encoded = new String(Base64.getEncoder().encode(encodedBytes));
+            txtEncoded.setText(encoded);
+            this.txtToEncode.setText("");
+        });
+        btnEncode.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 3) {
+                    // change look and feel
+                    try {
+                        UIManager.setLookAndFeel(UIManager.getLookAndFeel() instanceof FlatIntelliJLaf ? new FlatDarculaLaf() : new FlatIntelliJLaf());
+                        SwingUtilities.updateComponentTreeUI(pnl);
+                    } catch (UnsupportedLookAndFeelException ex) {
+                        ex.printStackTrace();
+                    }
+                } else if (e.getClickCount() == 4) {
+                    try {
+                        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                        SwingUtilities.updateComponentTreeUI(pnl);
+                    } catch (UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException | ClassNotFoundException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
 
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
 
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
         });
         btnDecode.addActionListener(e -> {
             String txtToDecode = this.txtToDecode.getText();
+            byte[] base64Bytes = Base64.getDecoder().decode(txtToDecode);
+            txtDecoded.setText(new String(base64Bytes));
+            this.txtToDecode.setText("");
+        });
 
+    }
 
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(new FlatIntelliJLaf());
+        SwingUtilities.invokeLater(() -> {
+            JFrame f = new JFrame();
+            f.setContentPane(new GUI().pnl);
+            f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            f.setPreferredSize(new Dimension(400, 400));
+            f.pack();
+            f.setVisible(true);
         });
     }
 }
--- src/me/nanjingchj/codec/Main.java
+++
@@ -1,7 +0,0 @@
-package me.nanjingchj.codec;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
